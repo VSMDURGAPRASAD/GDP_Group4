d61570679d65017d1fb0a35386b0feaa5e83761a
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2019-09-01 14:25:48 -0500

    Added login and Register.ejs Files in Views

--- codewords/server.js
+++ codewords/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
+
+app.set('view-engine','ejs');
+
+
 app.get('/',function(req,res){
     res.render('index.ejs');
 });
+
+app.get('/login',function(req,res){
+    res.render('login.ejs');
+
+})
+app.get('/register',function(req,res){
+    res.render('register.ejs');
+})
 app.listen(3000);
