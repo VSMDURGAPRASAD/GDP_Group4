f9bd0caac60803174883eee82d9bd14e81d5bafa
vamshi nuka <Vamshinuka@live.in>
2019-09-03 23:17:02 -0500

    Update login.js in controllers

--- codewords/controllers/login.js
+++ codewords/controllers/login.js
@@ -13,6 +13,21 @@ let signIn = (req,res) => {
         if(err){
             return res.json({ code: 200, message: 'Email id not registered!!'});
         }
+        console.log(User.isInstructor+"Instructor status signIn controller user");
+        return bcrypt.compare(body.password,User.password,(err,result) => {
+            if(result){
+                var newToken = jwt.sign({email: body.email, id: User.id },'codewordnwmsu',{expiresIn:  10000 * 3000 }).toString();
+                UserModel.updateOne({emailKey: body.email},{$set: {token: newToken}}, (err) =>{
+                    if(err){
+                        return res.json({ code: 200, message: 'Unable to generate and update Token'});
+                    }
+                    return res.json({ code: 200, message: 'Signed in successfully. Redirecting.', token: newToken, isInstructor: User.isInstructor });
+                })
+            }else{
+                return res.json({ code: 200, message: "Invalid User!!"})
+            }
+        })
 
-    }
+    })
 }
+
