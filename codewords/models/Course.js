4874e1cee04c766e12a7739cc112fe0fa31c13ef
vamshi nuka <Vamshinuka@live.in>
2019-09-15 16:33:27 -0500

    Update Course.js in models

--- codewords/models/Course.js
+++ codewords/models/Course.js
@@ -2,6 +2,20 @@ var mongoose = rewuire ('mongoose');
 var validator = require('validator');
 
 var courseSchema = new Schema ({
+    CourseName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+
+    StartDate: {
+        type: DataView
+    },
+
+    EndDate: {
+        type: DataView
+    }
+
     
 
 });
