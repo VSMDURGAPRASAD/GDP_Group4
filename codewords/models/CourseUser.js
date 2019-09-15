60377365b22291c56e33f3543b4cd23930f912c2
vamshi nuka <Vamshinuka@live.in>
2019-09-15 16:47:16 -0500

    Update CourseUser.js in models

--- codewords/models/CourseUser.js
+++ codewords/models/CourseUser.js
@@ -1,1 +1,35 @@
 var mongoose = require ('mongoose');
+var validator = require('validator');
+var mongoosePaginate = require('mongoose-paginate');
+
+
+var CourseUserSchema = new Schema ({
+
+    CourseName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    
+    email_id: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        validate:{
+            validator: (value) =>{
+                return validator.isEmail(value);
+            }        
+    },
+
+    first_name: {
+        type: String
+    },
+    last_name: {
+        type: String
+    },
+
+
+
+});
+
