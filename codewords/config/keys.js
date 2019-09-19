ed668808b71ac0e22b54ff9fc4803be7f57f5ae9
vamshi nuka <Vamshinuka@live.in>
2019-09-19 02:55:49 -0500

    Update .gitignore

--- codewords/config/keys.js
+++ codewords/config/keys.js
@@ -1,5 +1,4 @@
-dbPassword = 'mongodb+srv://Admin-Alpha:GDP-4@cluster0-igkzx.mongodb.net/test?retryWrites=true';
-
+dbPassword = 'mongodb+srv://Admin-Alpha:GDP-4@cluster0-igkzx.mongodb.net/test?retryWrites=true&w=majority';
 module.exports = {
     mongoURI: dbPassword
 }
