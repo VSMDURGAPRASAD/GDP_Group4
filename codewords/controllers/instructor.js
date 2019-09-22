b36b4d8c1928380a85a0ecfd271e06bde5b8eaec
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2019-09-22 12:45:37 -0500

    Add Instructor create course get request

---
+++ codewords/controllers/instructor.js
@@ -0,0 +1,61 @@
+/** 
+*  Instructor Course controller
+*  Handles requests related to instructors (see routes)
+
+*  Vinukonda Sai Manikanta Durga Prasad
+* 
+*
+*/
+const express = require('express')
+const api = express.Router()
+const LOG = require('../utils/logger.js')
+const find = require('lodash.find')
+const remove = require('lodash.remove')
+const Model = require('../models/instructor.js')
+const notfoundstring = 'instructor not found'
+
+// RESPOND WITH JSON DATA  --------------------------------------------
+
+// GET all JSON
+api.get('/findall',async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const data = await Model.find({})
+  res.send(JSON.stringify(data))
+})
+
+// GET one JSON by ID
+api.get('/findone/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.instructors.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring) }
+  res.send(JSON.stringify(item))
+})
+
+// RESPOND WITH VIEWS  --------------------------------------------
+
+// GET to this controller base URI (the default)
+api.get('/',async (req, res) => {
+
+  const data = await Model.find({})
+  console.log("sdf")
+  res.render('instructor/index.ejs',{val:data})
+})
+
+// api.get('/index', (req, res) => {
+//   res.render('instructor/index.ejs')
+// })
+
+// GET create
+api.get('/create', (req, res) => {
+  LOG.info(`Handling GET /create${req}`)
+  const item = new Model()
+  LOG.debug(JSON.stringify(item))
+  res.render('instructor/create',
+    {
+      title: 'Create instructor',
+      layout: 'layout.ejs',
+      instructor: item
+    })
+})
