a7f04cb99f9e48e0448afab2b802479721a4a3f1
sneha <S533984@nwmissouri.edu>
2019-09-22 20:52:27 -0500

    Add user model code

---
+++ codewords/config/passport.js
@@ -0,0 +1,12 @@
+passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+     
