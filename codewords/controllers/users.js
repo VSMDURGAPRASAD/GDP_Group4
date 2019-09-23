c819dc658b7cac47244a31a0a268e29425314a2d
Swathi Challagundla <43010470+Swathi4717@users.noreply.github.com>
2019-09-22 23:54:32 -0500

    Add users.js in controllers

---
+++ codewords/controllers/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+// Load User model
+const User = require('../models/User');
+const { forwardAuthenticated } = require('../config/auth');
+
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
