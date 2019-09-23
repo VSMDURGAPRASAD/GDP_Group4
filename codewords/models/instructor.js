b3d7c5bce63945e208d492788587484055ea07c2
sneha <S533984@nwmissouri.edu>
2019-09-22 21:57:49 -0500

    Add instructor schema sample data

--- codewords/models/instructor.js
+++ codewords/models/instructor.js
@@ -1,21 +1,17 @@
-/** 
-*  Instructor Page 
-*  Describes the features of a Instructor
-*
-* @author VSMDURGAPRASAD <dp.vinukonda@gmail.com>
-*
-*/
-
-// see <https://mongoosejs.com/> for more information
 const mongoose = require('mongoose')
 
 const InstructorSchema = new mongoose.Schema({
-  _courseName: { type: String, required: true },
-  startDate: { type: Date, required: true, default: Date.now() },
-  endDate: { type: Date, required: false },
-  intialSurveyLink: { type: String, },
-  endSurveyLink: { type: Boolean, default: false },
-  
-})
 
-module.exports = mongoose.model('Instructor',InstructorSchema)
+  _id: { type: Number, required: true },
+  coursename: {
+    type: String,
+    required: true,
+    unique: true
+    
+  },
+  startdate: {
+    type: String,
+    required: false
+    
+  },
+  
