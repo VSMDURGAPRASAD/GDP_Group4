6149bd93772ef8a42bc8134b399c3793a4f1de20
sneha <S533984@nwmissouri.edu>
2019-09-22 22:46:10 -0500

    Add user.js schema data

--- codewords/models/user.js
+++ codewords/models/user.js
@@ -1,55 +1,24 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
-    email_id: {
-        type: String,
-        // required: true,
-        //unique: true,
-        minlength: 5
-        },
-    first_name: {
-        type: String
-    },
-    last_name: {
-        type: String
-    },
-    password: {
-        type: String,
-        require: true,
-        minlength: 6
-    },
-    role: {
-        type: String,
-        enum: ['admin', 'student', 'instructor'],
-        require: true
-    }
-    // is_active: {
-    //     type: Boolean,
-    //     require: true,
-    //     default: false
-    // },
-    // last_login: {
-    //     type: Date
-    // },
-    // course: [
-    //     {
-    //         course_id: { type: String },
-    //         isRevealed: { type: Boolean }
-    //     }
-
-    // ],
-    // codewordset: [
-    //     {
-    //         codewordsetname: { type: String }
-    //     }
-    // ],
-    // created_at: {
-    //     type: Date
-    // },
-    // updated_at: {
-    //     type: Date,
-    // }
-
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-module.exports = mongoose.model('user',UserSchema)
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
