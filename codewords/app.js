e31dffbc24122a7b5375f19e427eb9e5f564e90c
vamshi nuka <Vamshinuka@live.in>
2019-11-04 13:43:07 -0600

    Modified app.js heroku

--- codewords/app.js
+++ codewords/app.js
@@ -105,7 +105,7 @@ require('./utils/seeder.js')(app)  // load seed data by passing in the app
 //  const env = app.get('env')
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`\nApp running at http://${host}:${port}/ in ${env} mode`)
+  // console.log(`\nApp running at http://${host}:${port}/ in ${env} mode`)
   console.log('Press CTRL-C to stop\n')
 })
 
