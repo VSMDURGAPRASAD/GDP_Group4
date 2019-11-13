d639cbdb2bda6c88bb1dc719022de2d11500365d
sneha <S533984@nwmissouri.edu>
2019-11-13 09:46:44 -0600

    Edit some part of code in instructor.js

--- codewords/controllers/instructor.js
+++ codewords/controllers/instructor.js
@@ -224,7 +224,7 @@ api.post('/save', async (req, res) => {
   LOG.info(`NEW ID ${req.body._id}`)
   console.log('form')
   var formss = new formidable.IncomingForm();
- // formss.uploadDir = "/app/codewords/uploads";
+  //formss.uploadDir = "/app/codewords/uploads";
   //formss.keepExtensions = true;
   //formss.maxFieldsSize = 10*1024*1024;
   //formss.multiples = false;
